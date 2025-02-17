6cea520490d97d757321582e5d7bc3b6ad5aa0fb
Chris Rose <offline@offby1.net>
2025-02-17 09:24:15 -0800

    fix: safely construct JSON from arbitrary input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+set -eu
+
+JSON_PAYLOAD=$(
+    jq -n \
+        --arg type "note" \
+        --arg title "$PB_TITLE" \
+        --arg body "$PB_TEXT" \
+        '{type: $type, title: $title, body: $body}'
+)
 
 curl \
---silent --show-error \
--X POST \
---header "Content-Type: application/json" \
---header "Authorization: Bearer ${PB_TOKEN}" \
--d "{\"type\":\"note\", \"title\":\"$PB_TITLE\", \"body\":\"$PB_TEXT\"}" \
-"https://api.pushbullet.com/v2/pushes"
+    --silent --show-error \
+    -X POST \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Bearer ${PB_TOKEN}" \
+    --data "$JSON_PAYLOAD" \
+    "https://api.pushbullet.com/v2/pushes"
